3d9d438bbec91116793be6449642190e66554810
Siddhant <siddhant.sharma1974@gmail.com>
2021-12-21 20:12:32 +0545

    Version-1.0.0 Changes recorded added map function

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ class mapper(object):
     def __init__(self,text):
         super(mapper,self).__init__()
         self.text=text
-        print(self.text)
+
+    def map(self):
+        self.words=self.text.split()
+        self.map_output=[]
+        for word in self.words:
+            self.map_output.append({word:1})
 
 mapper('Apple Ball and Cat')
