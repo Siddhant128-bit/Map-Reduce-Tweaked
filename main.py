72ca3490d9bf8d63c34ad2052e4de48c3c4b6c57
Siddhant <siddhant.sharma1974@gmail.com>
2021-12-21 20:20:35 +0545

    Version-1.0.1 Added Map and Reduce function inside both mapper and reducer class

--- main.py
+++ main.py
@@ -9,6 +9,25 @@ class mapper(object):
         self.map_output=[]
         for word in self.words:
             self.map_output.append({word:1})
+class reducer(object):
+    def __init__(self,map_list):
+        super(reducer,self).__init__()
+        self.map_list=map_list
 
+    def reduce(self):
+        self.reduce_output=[]
+        for length in range(0,len(self.map_list)):
+            current_pair=self.map_list[length]
+            current_word=current_pair.keys()
+            current_count=0
+            for i in self.map_list:
+                key=i.keys()
+                if current_word=key:
+                    current_count+=1
+            self.reduce_output[current_word[0]]=current_count
 
-mapper('Apple Ball and Cat')
+text=input("Enter the long text that you want to check: ")
+mapper_object=mapper(text)
+mapper_object.map()
+map_output=mapper_object.map_output
+print(map_output)
