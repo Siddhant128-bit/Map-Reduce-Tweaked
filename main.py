5ac2bdcc39cc590c35e83c2d0d8996cce1582555
Siddhant <siddhant.sharma1974@gmail.com>
2021-12-21 20:14:03 +0545

    Version-1.0.0 Changes recorded added map function added comments

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ class mapper(object):
         self.text=text
 
     def map(self):
-        self.words=self.text.split()
+        self.words=self.text.split() #Converting the text into list so that it can be traversed upon
         self.map_output=[]
         for word in self.words:
             self.map_output.append({word:1})
+
 
 mapper('Apple Ball and Cat')
